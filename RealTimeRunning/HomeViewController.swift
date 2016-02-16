076ed518ea8d81518122a91b432c2ccb8b871722
Scott Ashmore <sashmore@hi-res.net>
2016-02-16 22:26:39 +0000

    adding fb login

--- RealTimeRunning/HomeViewController.swift
+++ RealTimeRunning/HomeViewController.swift
@@ -7,22 +7,58 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        if (FBSDKAccessToken.currentAccessToken() == nil) {
+            
+            print("Not logged in...")
+            
+        } else {
+            
+            print("Logged in...")
+            
+        }
+        
+        let loginButton = FBSDKLoginButton()
+        
+        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+        loginButton.center = self.view.center
+        loginButton.delegate = self
+        
+        self.view.addSubview(loginButton)
+        
+    }
+    
+    // MARK: - Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        if error == nil {
+            
+            print("Login complete.")
+        
+            self.performSegueWithIdentifier("showNew", sender: self)
+
+        } else {
+            
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        
+        print("User logged out...")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        
-        super.didReceiveMemoryWarning()
-    
-        // Dispose of any resources that can be recreated.
-    
-    }
 
 
 }
