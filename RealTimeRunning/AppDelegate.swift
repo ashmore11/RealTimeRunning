0072ba42c9ae0e9a45b07ce305e9705ead703609
Scott Ashmore <dev.scottashmore@gmail.com>
2016-02-17 15:56:11 +0000

    updates

--- RealTimeRunning/AppDelegate.swift
+++ RealTimeRunning/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -51,6 +52,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        
+        loginManager.logOut()
+        
     }
 
 
--- RealTimeRunning/HomeViewController.swift
+++ RealTimeRunning/HomeViewController.swift
@@ -11,6 +11,13 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var fbProfileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
 
     override func viewDidLoad() {
         
@@ -19,20 +26,22 @@ class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
         if (FBSDKAccessToken.currentAccessToken() == nil) {
             
             print("Not logged in...")
+          
+            let loginButton = FBSDKLoginButton()
+          
+            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+            loginButton.center = self.view.center
+            loginButton.delegate = self
+          
+            self.view.addSubview(loginButton)
             
         } else {
             
             print("Logged in...")
             
+            getUserData(displayUserData)
+            
         }
-        
-        let loginButton = FBSDKLoginButton()
-        
-        loginButton.readPermissions = ["public_profile", "email", "user_friends"]
-        loginButton.center = self.view.center
-        loginButton.delegate = self
-        
-        self.view.addSubview(loginButton)
         
     }
     
@@ -43,8 +52,8 @@ class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
         if error == nil {
             
             print("Login complete.")
-        
-            self.performSegueWithIdentifier("showNew", sender: self)
+            
+            getUserData(displayUserData)
 
         } else {
             
@@ -59,7 +68,35 @@ class HomeViewController: UIViewController, FBSDKLoginButtonDelegate {
         
     }
 
+    func getUserData(callback: (String, String, String, UIImage) -> Void) {
+        
+        let graphRequest = FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields": "first_name, last_name, email, picture.type(large)"])
+        
+        graphRequest.startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            let firstName: String = (result.objectForKey("first_name") as? String)!
+            
+            let lastName: String = (result.objectForKey("last_name") as? String)!
+            
+            let email: String = (result.objectForKey("email") as? String)!
 
+            let imageURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            
+            let image: UIImage = UIImage(data: NSData(contentsOfURL: NSURL(string: imageURL)!)!)!
+            
+            callback(firstName, lastName, email, image)
+            
+        }
+        
+    }
+    
+    func displayUserData(firstName: String, lastName: String, email: String, image: UIImage) {
+        
+        self.nameLabel.text = "Name: \(firstName) \(lastName)"
+        self.emailLabel.text = "Email: \(email)"
+        self.fbProfileImage.image = image
+        
+    }
 
 }
 
