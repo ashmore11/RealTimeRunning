0990d904b80d583e1e1b5834db061b659246151b
Bob Ashmore <bob.ashmore@gmail.com>
2016-02-29 18:58:14 +0000

    text on graph

--- UIGraphView.swift
+++ UIGraphView.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
+enum textAlignTypes {
+    case ctAlignTopLeft
+    case ctAlignTopCenter
+    case ctAlignTopRight
+    case ctAlignCenterLeft
+    case ctAlignCenterCenter
+    case ctAlignCenterRight
+    case ctAlignBottomLeft
+    case ctAlignBottomCenter
+    case ctAlignBottomRight
+}
+
 class UIGraphView: UIView {
     var yMax:CGFloat = 0.0
     var dataPoints: [Double] = []
     var lineColor = UIColor.redColor()
+    var graphHeader = "Speed vs Time"
+    var numeralColor = UIColor.blackColor()
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
@@ -23,6 +37,8 @@ class UIGraphView: UIView {
             self.layer.borderColor = UIColor.blackColor().CGColor
             self.drawGrid(context)
             self.drawLine(context, pointsArray: dataPoints, lineColor: lineColor)
+            drawNormalText(graphHeader, context:context, origin:CGPointMake(self.bounds.width/2.0, 15), x1:0.0, y1:0.0, align: .ctAlignCenterCenter, fontName:"Helvetica", fontSize:20.0, textColor:numeralColor)
+
         }
     }
     
@@ -33,6 +49,9 @@ class UIGraphView: UIView {
             scaledPoint = (CGFloat(self.bounds.size.height) / self.yMax) * CGFloat(y)
             CGPathMoveToPoint(path, nil, 0.0, floor(self.bounds.size.height - scaledPoint)+0.5)
             CGPathAddLineToPoint(path,nil,CGFloat(self.bounds.size.width-1),floor(self.bounds.size.height - scaledPoint)+0.5)
+            let sx = String(format: "%02d",y)
+            drawNormalText(sx, context:context, origin:CGPointMake(4.0, floor(self.bounds.size.height - scaledPoint)+0.5), x1:0.0, y1:0.0, align: .ctAlignBottomLeft, fontName:"Helvetica", fontSize:10.0, textColor:numeralColor)
+
         }
         CGContextBeginPath(context)
         CGContextAddPath(context, path)
@@ -40,7 +59,19 @@ class UIGraphView: UIView {
         CGContextSetStrokeColorWithColor(context,UIColor.lightGrayColor().CGColor)
         CGContextStrokePath(context)
     }
-    
+
+    func measureLine(line: CTLineRef, context: CGContextRef) ->CGSize {
+        var textHeight: CGFloat = 0.0
+        var ascent:CGFloat = 0.0
+        var descent:CGFloat = 0.0
+        var leading:CGFloat = 0.0
+        var width:Double = 0.0
+        
+        width = CTLineGetTypographicBounds(line, &ascent,  &descent, &leading)
+        textHeight = floor(ascent * 0.8)
+        return CGSizeMake(ceil(CGFloat(width)), ceil(textHeight))
+    }
+
     func drawLine(context:CGContextRef, pointsArray:[Double], lineColor:UIColor)
     {
         if pointsArray.count == 0 {
@@ -74,6 +105,70 @@ class UIGraphView: UIView {
         CGContextSetLineWidth(context,1.0)
         CGContextSetStrokeColorWithColor(context,lineColor.CGColor)
         CGContextStrokePath(context)
+    }
+    
+    func drawNormalText(text:String, context:CGContextRef, origin:CGPoint, x1:CGFloat, y1:CGFloat, align:textAlignTypes, fontName:String, fontSize:CGFloat, textColor:UIColor) {
+        CGContextSaveGState(context)
+        
+        //let shadowColor = UIColor(red:0.151, green:0.152, blue:0.151, alpha:1.000)
+        //CGContextSetShadowWithColor(context, CGSizeMake(1, 1), 0, shadowColor.CGColor)
+        CGContextTranslateCTM(context, origin.x, origin.y) // move origin to center
+        CGContextScaleCTM(context, 1.0, -1.0) // Reverse Y axis only
+        
+        // Create an attributed string
+        let fontRef = CTFontCreateWithName(fontName, fontSize, nil)
+        let attributes = [ NSFontAttributeName: fontRef, NSForegroundColorAttributeName: textColor ]
+        let attString = NSAttributedString(string: text, attributes: attributes)
+        
+        // Create a text line from the attributed string
+        let line:CTLineRef = CTLineCreateWithAttributedString(attString)
+        let runArray = ((CTLineGetGlyphRuns(line) as [AnyObject]) as! [CTRunRef])
+        let lineMetrics:CGSize = measureLine(line,context:context)
+        for runIndex in 0..<CFArrayGetCount(runArray) {
+            let run: CTRunRef = runArray[runIndex]
+            var textMatrix:CGAffineTransform = CTRunGetTextMatrix(run)
+            switch (align) {
+            case .ctAlignTopLeft:
+                textMatrix.tx = x1
+                textMatrix.ty = y1 - lineMetrics.height
+                
+            case .ctAlignTopCenter:
+                textMatrix.tx = x1 + (lineMetrics.width / 2.0)
+                textMatrix.ty = y1 - lineMetrics.height
+                
+            case .ctAlignTopRight:
+                textMatrix.tx = x1 + lineMetrics.width
+                textMatrix.ty = y1 - lineMetrics.height
+                
+            case .ctAlignCenterLeft:
+                textMatrix.tx = x1
+                textMatrix.ty = y1 - (lineMetrics.height / 2.0)
+                
+            case .ctAlignCenterCenter:
+                textMatrix.tx = x1 - (lineMetrics.width / 2.0)
+                textMatrix.ty = y1 - (lineMetrics.height / 2.0)
+                
+            case .ctAlignCenterRight:
+                textMatrix.tx = x1 - lineMetrics.width
+                textMatrix.ty = y1 - (lineMetrics.height / 2.0)
+                
+            case .ctAlignBottomLeft:
+                textMatrix.tx = x1
+                textMatrix.ty = y1
+                
+            case .ctAlignBottomCenter:
+                textMatrix.tx = x1 - (lineMetrics.width / 2.0)
+                textMatrix.ty = y1
+                
+            case .ctAlignBottomRight:
+                textMatrix.tx = x1 - lineMetrics.width
+                textMatrix.ty = y1
+                
+            }
+            CGContextSetTextMatrix(context, textMatrix)
+            CTRunDraw(run, context, CFRangeMake(0, 0))
+        }
+        CGContextRestoreGState(context)
     }
 
 }
