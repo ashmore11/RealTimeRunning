1d6da8da49af2213cccc7c1bd0a3a747ad771da8
Bob Ashmore <bob.ashmore@gmail.com>
2016-03-14 13:38:42 +0000

    Changed background switcher view to use constraints

--- RealTimeRunning/IOSBackgroundSwitcherView.swift
+++ RealTimeRunning/IOSBackgroundSwitcherView.swift
@@ -34,25 +34,65 @@ class IOSBackgroundSwitcherView: UIView {
         
         if let image = UIImage(named: "launchImage.png") {
             let imageView = UIImageView(image: image)
-            imageView.frame = CGRect(x: (self.bounds.width/2.0) - 150, y: (self.bounds.height/2.0) - 150, width: 300, height: 300)
             self.addSubview(imageView)
+            self.setupImageConstraints(imageView, mainView:self)
         }
         
-        let headerLabel = UILabel(frame: CGRectMake(0, 0, self.bounds.width, 50))
-        headerLabel.center = CGPointMake(self.bounds.width/2.0, 60)
+        let headerLabel = UILabel(frame: CGRectMake(0, 0, 0, 0))
         headerLabel.textAlignment = NSTextAlignment.Center
         headerLabel.text = "RealTimeRunning"
         headerLabel.font = headerLabel.font.fontWithSize(38)
         self.addSubview(headerLabel)
+        self.setupHeaderLabelConstraints(headerLabel, mainView:self)
         
-        
-        let copyrightLabel = UILabel(frame: CGRectMake(0, 0, self.bounds.width, 50))
-        copyrightLabel.center = CGPointMake(self.bounds.width/2.0, self.bounds.height - 20)
+        let copyrightLabel = UILabel(frame: CGRectMake(0, 0, 0, 0))
         copyrightLabel.textAlignment = NSTextAlignment.Center
         copyrightLabel.text = "Copyright © Scott Ashmore 2016"
         copyrightLabel.font = copyrightLabel.font.fontWithSize(17)
         self.addSubview(copyrightLabel)
-       
-        
+        self.setupCopyrightLabelConstraints(copyrightLabel, mainView:self)
     }
+    
+    func setupHeaderLabelConstraints(subView:UIView, mainView:UIView) {
+        subView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leadingConstraint = NSLayoutConstraint(item: subView, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1, constant: 0)
+        mainView.addConstraint(leadingConstraint)
+
+        let trailingConstraint = NSLayoutConstraint(item: subView, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1, constant: 0)
+        mainView.addConstraint(trailingConstraint)
+
+        let topConstraint = NSLayoutConstraint(item: subView, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1, constant: 40)
+        mainView.addConstraint(topConstraint)
+    }
+    
+    func setupCopyrightLabelConstraints(subView:UIView, mainView:UIView) {
+        subView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let leadingConstraint = NSLayoutConstraint(item: subView, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1, constant: 0)
+        mainView.addConstraint(leadingConstraint)
+        
+        let trailingConstraint = NSLayoutConstraint(item: subView, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1, constant: 0)
+        mainView.addConstraint(trailingConstraint)
+        
+        let bottomConstraint = NSLayoutConstraint(item: subView, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1, constant: -10)
+        mainView.addConstraint(bottomConstraint)
+    }
+
+    func setupImageConstraints(subView:UIView, mainView:UIView) {
+        subView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let widthConstraint = NSLayoutConstraint(item:subView, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 300)
+        mainView.addConstraint(widthConstraint)
+        
+        let heightConstraint = NSLayoutConstraint(item: subView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 300)
+        mainView.addConstraint(heightConstraint)
+        
+        let top:NSLayoutConstraint = NSLayoutConstraint(item: subView, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.CenterX, multiplier: 1, constant: 0)
+        mainView.addConstraint(top)
+        
+        let bottom:NSLayoutConstraint = NSLayoutConstraint(item: subView, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: mainView, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+        mainView.addConstraint(bottom)
+    }
+
 }
